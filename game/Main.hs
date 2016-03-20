5006c5aa7784bb993f8f5e6746de40fb4ca6a852
Maximilian Algehed <m.algehed@gmail.com>
2016-03-20 21:42:57 +0100

    Implemented a basic interface

--- game/Main.hs
+++ game/Main.hs
@@ -1,4 +1,28 @@
 module Main where
+import Data.Matrix
 import GoWageWar.Board
+import GoWageWar.Graphics
+import Brick.Main
+import Brick.AttrMap
+import Graphics.Vty
 
-main = undefined
+theApp :: App Board Event
+theApp = App {appDraw = (:[]) . drawBoard,
+              appChooseCursor = const (const Nothing),
+              appHandleEvent = (\b _ -> halt b),
+              appStartEvent = return,
+              appLiftVtyEvent = id,
+              appAttrMap = const $ attrMap defAttr attributes
+             }
+
+board = endTurn $ fromLists [
+    [(Nothing, 0), (Nothing, 0),                (Nothing, 0),  (Nothing, 0),          (Nothing, 0),                (Nothing, 0)],
+    [(Nothing, 0), (Nothing, 0),                (Nothing, 0),  (Nothing, 0),          (Nothing, 0),                (Nothing, 0)],
+    [(Nothing, 0), (Just (Keep, Blue), 0),      (Nothing, 0),  (Nothing, 0),          (Just (Watchtower, Red), 0), (Nothing, 0)],
+    [(Nothing, 0), (Just (Watchtower, Blue), 0),(Nothing, 0),  (Just (Keep, Red), 0), (Nothing, 0),                (Nothing, 0)],
+    [(Nothing, 0), (Just (Wall, Blue), 0),      (Nothing, 0),  (Nothing, 0),          (Nothing, 0),                (Nothing, 0)],
+    [(Just (Wall, Red), 0), (Nothing, 0),                (Nothing, 0),  (Nothing, 0),          (Nothing, 0),                (Nothing, 0)],
+    [(Just (Watchtower, Red), 0), (Just (Wall, Red), 0),                (Nothing, 0),  (Nothing, 0),          (Nothing, 0),                (Just (Wall, Red), 0)]
+   ]
+
+main = defaultMain theApp board
--- src/GoWageWar/Graphics.hs
+++ src/GoWageWar/Graphics.hs
@@ -1,32 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
-module GoWageWare.Graphics
+module GoWageWar.Graphics
     (
-        drawBoard
+        drawBoard,
+        attributes
     ) where
 import GoWageWar.Board
 import Data.Matrix
 import Brick.Widgets.Core
 import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
+import Brick.AttrMap
 import Brick.Types
+import Brick.Util
+import Graphics.Vty.Attributes
+
+-- | The attributes for red and blue
+attributes :: [(AttrName, Attr)]
+attributes = [("red", red `on` black), ("blue", blue `on` black)]
 
 -- | Create a widget representing the board state
 drawBoard :: Board -> Widget
 drawBoard board = withBorderStyle unicodeBold
+                $ border
                 $ foldl1 (<=>)
                 $ map (foldl1 (<+>))
                 $ toLists
                 $ fmap toWidget board
 
+padding :: Int
+padding = 0
+
 -- | Create a widget representing a cell
 toWidget :: Cell -> Widget
 toWidget (Nothing, x)
-    | x == 0                       = str (show x)
-    | signum x == colourSignum Red = withAttr "red" $ str (show x) 
-    | otherwise                    = withAttr "blue" $ str (show x)
+    | x == 0                       = str "." -- No influence from either player
+    | signum x == colourSignum Red = padAll padding $ withAttr "red" $ str (show (abs x)) -- Influence from red
+    | otherwise                    = padAll padding $ withAttr "blue" $ str (show (abs x)) -- Influence from blue
 toWidget (Just (t, colour), _)
-    | colour == Red                = withAttr "red" $ str $ towerStr t
-    | otherwise                    = withAttr "blue" $ str $ towerStr t
+    | colour == Red                = padAll padding $ withAttr "red" $ str $ towerStr t -- Red tower
+    | otherwise                    = padAll padding $ withAttr "blue" $ str $ towerStr t -- Blue tower
     where
         towerStr Wall       = "⋄"
         towerStr Watchtower = "∆"
